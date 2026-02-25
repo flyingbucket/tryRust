bf0ec0f28c468b49045ce760090bf33919f5a4c3
flyingbucket <menglinli598@gmail.com>
2026-02-25 17:48:39 +0800

    update generic

--- pkgs/generic/src/main.rs
+++ pkgs/generic/src/main.rs
@@ -1,8 +1,18 @@
+use std::cmp::Ordering;
+use std::fmt;
+
 fn main() {
-    println!("Hello, world!");
+    let points = [
+        Point(0.0, 0.0),
+        Point(0.0, 1.0),
+        Point(12.0, -3.34),
+        Point(17.0, -0.28),
+    ];
+    let largest_mod = largest(&points);
+    println!("The farest point from origin is {largest_mod}");
 }
 
-fn largest<T>(list: &[T]) -> &T {
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
     let mut lgt = &list[0];
     for element in list {
         if element > lgt {
@@ -12,14 +22,20 @@ fn largest<T>(list: &[T]) -> &T {
     lgt
 }
 
-#[derive(Debug)]
-struct Point<T> {
-    x: T,
-    y: T,
+#[derive(PartialEq)]
+struct Point(f32, f32);
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let left_len = self.0 * self.0 + self.1 * self.1;
+        let right_len = other.0 * other.0 + other.1 * other.1;
+
+        left_len.partial_cmp(&right_len)
+    }
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Point({}, {})", self.0, self.1)
     }
 }
