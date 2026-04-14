1d79ced0fcfbad2fd112cd929f1bcbabacd79c28
flyingbucket <2311067@mail.nankai.edu.cn>
2026-04-14 14:33:29 +0800

    bst: LCG random generator

--- pkgs/BST/benches/bst_benchmark.rs
+++ pkgs/BST/benches/bst_benchmark.rs
@@ -2,19 +2,17 @@ use bst::Tree;
 use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
-// 还是那个生成伪随机数的辅助函数
-fn generate_random_data(count: usize) -> Vec<i32> {
+fn linear_congruential_generator(mut seed: u32, count: usize) -> Vec<i32> {
     let mut data = Vec::with_capacity(count);
-    let mut state: u32 = 12345;
     for _ in 0..count {
-        state = state.wrapping_mul(1664525).wrapping_add(1013904223);
-        data.push((state % 10000) as i32);
+        seed = seed.wrapping_mul(1664525).wrapping_add(1013904223);
+        data.push((seed % 10000) as i32);
     }
     data
 }
 
 fn bench_tree_inserts(c: &mut Criterion) {
-    let nums = generate_random_data(1000); // 测试插入 1000 个节点
+    let nums = linear_congruential_generator(42, 10_000);
 
     // 创建一个测试组，方便在报告中将两者放在同一张图表里对比
     let mut group = c.benchmark_group("BST Insertions");
@@ -28,7 +26,7 @@ fn bench_tree_inserts(c: &mut Criterion) {
                 tree = tree.insert(n);
             }
             // 使用 black_box 防止编译器过度优化（比如发现 tree 没被使用就直接把循环删了）
-            // black_box(tree);
+            black_box(tree);
         })
     });
 
@@ -39,7 +37,7 @@ fn bench_tree_inserts(c: &mut Criterion) {
             for &n in &nums {
                 tree.insert_mut(n);
             }
-            // black_box(tree);
+            black_box(tree);
         })
     });
 
